3b9e2ca6cdbefc193e44a6cec1876f0b71d451b0
FleyCan <FleyCan@users.noreply.github.com>
2026-07-27 17:26:00 +0200

    Added Element
    Class that is going to represent one element of the Minefield

---
+++ inc/Element.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <bitset>
+#include <cstdint>
+#include <ostream>
+
+struct Element {
+
+	std::bitset<7> data{};
+
+	Element(std::uint8_t value) {
+		data = value | 1;
+	}
+
+	bool hasMine() {
+		return data[6];
+	}
+
+	bool hasFlag() {
+		return data[5];
+	}
+
+	bool isUncovered() {
+		return data[4];
+	}
+
+	int getValue() { //std::uint8_t doesn't work
+		std::uint8_t mask = (1 << 4) - 1; //0b00001111
+
+		std::uint8_t sum = data.to_ulong() & mask;
+
+		return sum;
+	}
+
+	void setMine(bool state) {
+		data[6] = state;
+	}
+
+	void setFlag(bool state) {
+		data[5] = state;
+	}
+
+	void setUncovered(bool state) {
+		data[4] = state;
+	}
+
+	void setValue(std::uint8_t value) {
+		if(value > 8) return;
+
+		std::uint8_t mask = (1 << 4) - 1; //0b00001111
+
+		data = (data.to_ulong() & ~mask) | value;
+	}
+
+	friend
+	std::ostream& operator<<(std::ostream& os, Element const& element)
+	{
+		os << element.data;
+		return os;
+	}
+
+};
